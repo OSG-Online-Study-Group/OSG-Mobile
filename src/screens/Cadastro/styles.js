695d4248762141e589972b0f274e413152f93f23
juanoliveira0020 <juanoliveira200741@gmail.com>
2026-03-31 16:15:56 -0300

    feat: refine styles in Cadastro screen for improved layout and alignment

--- src/screens/Cadastro/styles.js
+++ src/screens/Cadastro/styles.js
@@ -1,41 +1,47 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-flex: 1;
-background-color: #2C173C;
-justify-content: center;
-align-items: center;
-padding: 20px;
-`
+  flex: 1;
+  background-color: #2C173C;
+  align-items: center;
+  padding: 20px;
+`;
+
 export const Logo = styled.Image`
-margin-top: -90px;
-`
+  margin-top: 40px;
+  width: 400px;
+  height: 400px;
+`;
 
 export const Title = styled.Text`
-font-size: 30px;
-color: #42A4C5;
-margin-bottom: 50px;
-margin-top: -60px;
-`
+  font-size: 30px;
+  color: #42A4C5;
+  margin-bottom: 40px;
+  margin-bottom: 10px;
+`;
+
 export const Input = styled.TextInput`
   width: 80%;
   background-color: #724274;
   padding: 15px;
   border-radius: 20px;
-  margin: 15px;
+  margin: 12px;
   font-size: 15px;
-`
+  color: #fff;
+`;
 
 export const Button = styled.TouchableOpacity`
-background-color: #42A4C5;
-border-radius: 20px;
-padding: 10px;
-margin: 15px;
-width: 30%;
-`
+  background-color: #42A4C5;
+  border-radius: 25px;
+  padding: 12px;
+  margin: 20px;
+  width: 40%;
+  align-items: center;
+`;
 
 export const ButtonText = styled.Text`
-font-size: 16px;
-color: #724274;
-text-align: center;
-`
+  font-size: 16px;
+  color: #42254E;
+  text-align: center;
+  font-weight: bold;
+`;
